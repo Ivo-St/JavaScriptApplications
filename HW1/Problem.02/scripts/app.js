554bd67920445bc0688525a6340cae4aa51d3b6c
ivostoianov <ivostoianov94@gmail.com>
2015-04-03 19:01:02 +0300

    Completed Problem 02

--- HW1/Problem.02/scripts/app.js
+++ HW1/Problem.02/scripts/app.js
@@ -1,1 +1,15 @@
 var switchColor = switchColor || {};
+
+(function () {
+    function changeColor(color, className) {
+        $('.' + className).css('background-color', color);
+    }
+
+    (function () {
+        $('#inputButton').click(function () {
+            var color = $('#color').val();
+            var className = $('#insertClass').val();
+            changeColor(color, className);
+        });
+    }());
+}());
