b2b3791bdd1b2de518207a7dcad259c1e161fac6
ivostoianov <ivostoianov94@gmail.com>
2015-04-04 14:05:33 +0300

    Finished Problem 03

--- HW1/Problem.03/scripts/app.js
+++ HW1/Problem.03/scripts/app.js
@@ -1,3 +1,35 @@
 (function () {
+    var jsonInput = "[{\"manufacturer\":\"BMW\",\"model\":\"E92 320i\",\"year\":2011,\"price\":50000,\"class\":\"Family\"}, {\
+            \"manufacturer\":\"Porsche\",\"model\":\"Panamera\",\"year\":2012,\"price\":100000,\"class\":\"Sport\"}, {\
+                \"manufacturer\":\"Peugeot\",\"model\":\"305\",\"year\":1978,\"price\":1000,\"class\":\"Family\"}]";
 
+    jsonInput = JSON.parse(jsonInput);
+    var keys = Object.keys(jsonInput[0]);
+    var table = $('<table>');
+    table.css('border', '1px solid');
+    $('body').append(table);
+    $('table').append('<tr>');
+    keys.forEach(function (key) {
+        var td = $('<td>');
+        td.css('border', '1px solid');
+        td.css('text-align', 'center');
+        td.css('background', 'green');
+        td.text(key);
+        $('tr').append(td);
+
+    });
+
+    jsonInput.forEach(function (element) {
+        var tr = $('<tr>');
+        $('table').append(tr);
+        keys.forEach(function (key) {
+            var td = $('<td>');
+            td.css('border', '1px solid');
+            td.css('text-align', 'center');
+            td.text(element[key]);
+            $(tr).append(td);
+        });
+    });
+
+    //todo split the code in functions
 }());
